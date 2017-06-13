ebb0cddf086c5a3abe3337b2eaa6fc952bb1ab5e
Robbie Xie <619835217@qq.com>
2017-06-13 14:27:40 +0800

    modify property filename

---
+++ ConfigServer/configclient/src/main/java/com/example/demo/ConfigclientApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigclientApplication.class, args);
+	}
+
+	@Value("${from}")
+	String from;
+
+	@RequestMapping(value = "/from")
+	public String from(){
+		return from;
+	}
+}
--- EurekaClient/src/main/java/com/example/demo/ComputeController.java
+++ EurekaClient/src/main/java/com/example/demo/ComputeController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +22,15 @@ public class ComputeController {
     @Autowired
     private DiscoveryClient client;
 
+    @Value("${from}")
+    String gitVersion;
+
     @RequestMapping(value = "/add" , method = RequestMethod.GET)
     public String add(@RequestParam String name, @RequestParam int a, @RequestParam int b){
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
         logger.info("/add, host: "+ instance.getHost()+",service_id: "+instance.getServiceId()+",result: "+r);
-        return "/add, From: "+ name +" ,host: "+ instance.getHost()+":"+instance.getPort()+",service_id: "+instance.getServiceId()+",result: "+r;
+        return "/add, From: "+ name +" ,host: "+ instance.getHost()+":"+instance.getPort()+",service_id: "+instance.getServiceId()+",result: "+r +" gitVersion: " +gitVersion;
     }
 
     @RequestMapping(value = "/info" , method = RequestMethod.GET)
